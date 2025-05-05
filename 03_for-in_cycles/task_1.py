71a15e0901a18ac1e4109ee65b4472e23c79f587
Nurullaev Mukhammad <nurullaevwork@gmail.com>
2025-05-05 21:30:23 +0500

    learn: for-in cycles

---
+++ 03_for-in_cycles/task_1.py
@@ -0,0 +1,13 @@
+"""
+📌 Description:
+Напишите программу, которая выводит текст «Python is awesome!» (без кавычек) 10 раз.
+
+💡 Example output:
+Sample Input -> Python is awesome!
+Sample Output -> Python is awesome! (10x)
+
+✅ My solution:
+"""
+
+for i in range(10):
+    print('Python is awesome!')
---
+++ 03_for-in_cycles/task_2.py
@@ -0,0 +1,23 @@
+"""
+📌 Description:
+Напишите программу, которая использует ровно три цикла for для печати следующей последовательности символов:
+
+💡 Example output:
+Sample Input -> 
+Sample Output -> 
+
+✅ My solution:
+"""
+
+for i in range(6):
+  print('AAA')
+
+for i in range(5):
+  print('BBBB')
+
+print('E')
+
+for i in range(9):
+  print('TTTTT')
+  
+print('G')
